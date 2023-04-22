00ed6c583fb77ce3ebcdee44ef1a57a7845d066f
DavidLakeT <djcardonan@eafit.edu.co>
2023-04-22 11:33:07 -0500

    Spring Boot setup

---
+++ src/test/java/me/DavidLake/zenith/ZenithApplicationTests.java
@@ -0,0 +1,13 @@
+package me.DavidLake.zenith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
