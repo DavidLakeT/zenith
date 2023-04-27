45bdaf24d4e9005b83e6ffe217dc8b98ef374fb3
DavidLakeT <djcardonan@eafit.edu.co>
2023-04-26 20:13:08 -0500

    Fixed import

--- src/main/java/me/davidlake/zenith/service/ProductService.java
+++ src/main/java/me/davidlake/zenith/service/ProductService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import jakarta.persistence.EntityNotFoundException;
 import me.davidlake.zenith.dto.model.ProductFilterDTO;
 import me.davidlake.zenith.model.Product;
 import me.davidlake.zenith.repository.ProductRepository;
